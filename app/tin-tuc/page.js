e2664580af95957659c7927f04df45161fb3edea
Truong Tan Dat <truongtandat1409@gmail.com>
2025-06-16 00:40:26 +0700

    aaaa

--- app/tin-tuc/page.js
+++ app/tin-tuc/page.js
@@ -3,7 +3,7 @@ import React from "react";
 const News = () => {
   return (
     <div className="">
-      <h1 className="text-4xl font-bold">Tin tức</h1>
+      <h1 className="text-4xl font-bold">Tin tức 2</h1>
     </div>
   );
 };
